9b5a5612144f738358c9efb7d7e18e1ff21e3608
Kamil Michalski <kmichalski@outlook.com>
2024-07-30 15:25:27 +0200

    project init with the dashboard page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: DashboardPageComponent
+  }
+];
